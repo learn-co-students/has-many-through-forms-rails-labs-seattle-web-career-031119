c19969b245be38ba720aabe2bc22e42d718af3f0
Mera-Stackhouse <mera.stackhouse@gmail.com>
2019-04-10 16:21:06 -0700

    Done.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,25 @@
 class CommentsController < ApplicationController
 
   def create
-    comment = Comment.create(comment_params)
-    redirect_to comment.post
+    if !params[:comment][:user_id].empty?
+      comment = Comment.create(other_params)
+      redirect_to post_path(comment.post)
+    else
+      # byebug
+      comment = Comment.create(comment_params)
+      redirect_to post_path(comment.post)
+    end
   end
 
   private
 
   def comment_params
-    params.require(:comment).permit(:content, :post_id, :user_id, user_attributes:[:username])
+    params.require(:comment).permit(:content, :post_id, user_attributes:[:username])
   end
+
+  def other_params
+    params.require(:comment).permit(:content, :post_id, :user_id)
+  end
+
+
 end
